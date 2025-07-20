12836371e90d9e439a46e6eb9beaf27e22bb39b2
Timmy Jose <zoltan.jose@gmail.com>
2025-07-20 17:50:55 +0530

    Initial commit

---
+++ HelloWorld.m
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+
+// Run with: `clang -std=c99 -pedantic -Wall -Werror -Wextra -lobjc -framework Foundation HelloWorld.m -o HelloWorld`
+int main(void) 
+{
+  NSLog(@"Hello, world!");
+}
